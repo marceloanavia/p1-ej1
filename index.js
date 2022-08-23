5c0d0118442a36133d6b887a51d01f8d29ced57d
marceloanavia <111400098+marceloanavia@users.noreply.github.com>
2022-08-23 08:40:22 -0300

    modifique el .html y .js

--- index.js
+++ index.js
@@ -1,58 +1,24 @@
 "use strict";
 
 /* TODO */
-/*
-var numeroUno = parseInt(prompt("Ingresar principio del rango numérico"));
-var numeroDos = parseInt(prompt("Ingresar final del rango numérico"));
-var numeroRango = parseInt(prompt("Ingresar número en el rango numérico"));
+
+var rangoInferior = parseInt(prompt("Ingresar principio del rango numérico"));
+var rangoSuperior = parseInt(prompt("Ingresar final del rango numérico"));
+var rangoValor = parseInt(prompt("Ingresar número en el rango numérico"));
 
 
-if (numeroUno > numeroDos){
+if (rangoInferior >= rangoSuperior){
     alert("El rango no puede ser invertido");
-} else if ((numeroRango >= numeroUno) && (numeroRango <= numeroDos)){
-    if ((numeroRango % 2) == 0){
+} else if ((rangoValor >= rangoInferior) && (rangoValor <= rangoSuperior)){
+    if ((rangoValor % 2) == 0){
         alert("el número está en el rango y es par");
     } else {
         alert("el número está en el rango");
     }
-} else if ((numeroRango < numeroUno) || (numeroRango > numeroDos)){
-    if ((numeroRango % 2) == 0){
+} else if ((rangoValor < rangoInferior) || (rangoValor > rangoSuperior)){
+    if ((rangoValor % 2) == 0){
         alert("el número no está en el rango");
     } else {
         alert("el número no está en el rango y es impar");
     }
 }
-*/
-
-var numeroUno = 0;
-var numeroDos = 0;
-var numeroRango = 0;
-var contador = 1;
-
-do {
-    numeroUno = parseInt(prompt("Ingresar principio del rango numérico"));
-    numeroDos = parseInt(prompt("Ingresar final del rango numérico"));
-
-    if (numeroUno > numeroDos){
-        alert("El rango no puede ser invertido");
-    } else {
-        numeroRango = parseInt(prompt("Ingresar número en el rango numérico"));
-        if ((numeroRango >= numeroUno) && (numeroRango <= numeroDos)){
-            if ((numeroRango % 2) == 0){
-                alert("el número está en el rango y es par");
-            } else {
-                alert("el número está en el rango");
-            }
-        } else if ((numeroRango < numeroUno) || (numeroRango > numeroDos)){
-            if ((numeroRango % 2) == 0){
-                alert("el número no está en el rango");
-            } else {
-                alert("el número no está en el rango y es impar");
-            }
-        }
-    }
-
-    
-    contador++
-
-} while (contador == 1)
